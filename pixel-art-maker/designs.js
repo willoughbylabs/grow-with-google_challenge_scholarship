54b327119888d03eeece8ff3afb8af9b8abd60eb
Andrew Willoughby <30395608+willoughbylabs@users.noreply.github.com>
2018-04-10 22:11:30 -0500

    Add set pixel color on click feature

--- pixel-art-maker/designs.js
+++ pixel-art-maker/designs.js
@@ -1,30 +1,33 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+// Execute script after page loads
 $(function() {
-
-// Clear the grid and create new grid
     $('#sizePicker').on('submit', function(event) {
+        // Clear the current grid
         $('#pixelCanvas').children().remove();
+        // When size is submitted by the user, call makeGrid()
         makeGrid();
+        // Prevent form submission from refreshing page and clearing grid
         event.preventDefault();
     });
 
     function makeGrid() {
-    // Get width and height values for creating grid
+        // Get user values for grid size and pixel color
         let gridHeight = $('#inputHeight').val();
         let gridWidth = $('#inputWidth').val();
 
-    // loop through grid values to create rows and cells
-    let i = 0;
-    while (i < gridHeight) {
-        $('#pixelCanvas').append('<tr></tr>');
-        i++;
+        // Draw grid based on user values
+        let i = 0;
+        while (i < gridHeight) {
+            $('#pixelCanvas').append('<tr></tr>');
+            i++;
+        };
+            for (var j = 0; j < gridWidth; j++) {
+                $('tr').append('<td></td>');
+            };
     }
-        for (var j = 0; j < gridWidth; j++) {
-            $('tr').append('<td></td>');
-       }
-   }
+    // Set pixel color on click based on user selection
+   $('#pixelCanvas').on('click', 'td', function() {
+        let pixelColor = $('#colorPicker').val();
+        $(this).css('background-color', pixelColor);
+    })
 })
 
