b62356170715eba969cb89f64661b3436041b754
Andrew Willoughby <30395608+willoughbylabs@users.noreply.github.com>
2018-04-10 20:42:13 -0500

    Add clear and create grid when size is submitted by user

--- pixel-art-maker/designs.js
+++ pixel-art-maker/designs.js
@@ -2,9 +2,27 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+$(function() {
 
-function makeGrid() {
+// Clear the grid and create new grid
+    $('#sizePicker').on('submit', function(event) {
+        $('#pixelCanvas').children().remove();
+        makeGrid();
+        event.preventDefault();
+    });
 
-// Your code goes here!
+    function makeGrid() {
+    // Get width and height values for creating grid
+        let gridHeight = $('#inputHeight').val();
+        let gridWidth = $('#inputWidth').val();
 
-}
+    // loop through grid values to create rows and cells
+        for (var i = 1; i <= gridHeight; i++) {
+            $('#pixelCanvas').append('<tr></tr>')
+        };
+        for (var j = 0; j < gridWidth; j++) {
+            $('tr').append('<td></td>');
+        };
+    }
+})
+
